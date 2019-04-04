bfe092f1b0d574a63ccf937d1cdbc402131061a2
barbara.jamroga <barbara.jamroga@microsoft.wsei.edu.pl>
2019-04-04 09:45:43 +0200

    change

--- e5 zest2/Program.cs
+++ e5 zest2/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace e5_zest2
+namespace ConsoleApp5
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int n;
+            Console.Write("Enter n: ");
+            string text = Console.ReadLine();
+            if (!Int32.TryParse(text, out n))
+                throw new ArgumentException("n is not an integer!\n");
+            int fctrl = Factorial(n);
+            Console.WriteLine("Factorial of {0} equals: {1}", n, fctrl);
+
+            Console.ReadKey();
+        }
+
+            static int Factorial(int mm)
+        {
+            int i = 1;
+            int result = 1;
+            while (i <= mm)
+                result *= i++;
+
+            return result;
         }
     }
 }
